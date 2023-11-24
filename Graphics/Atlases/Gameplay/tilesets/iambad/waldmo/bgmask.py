4a8aa41d30499ecda853766539bcb46fe4363038
wlaub <goldcray@gmail.com>
2023-11-24 11:54:18 -0700

    generated background tilesets

--- Graphics/Atlases/Gameplay/tilesets/iambad/waldmo/bgmask.py
+++ Graphics/Atlases/Gameplay/tilesets/iambad/waldmo/bgmask.py
@@ -1,17 +1,45 @@
 import sys
 import random
 
-from PIL import Image
+from PIL import Image, ImageChops
 
 mask_file = sys.argv[1]
 
 mask = Image.open(mask_file)
 mask = mask.getchannel('R')
 
-for filename in ['brightblue.png']:
-    base = filename.split('.')[0]
+files = {
+'darkblue',
+'brightblue',
+'vividblue',
+'darkyellow',
+'brightyellow',
+'vividyellow',
+'emptyred',
+'brightred',
+'vividred',
+'sinisterred',
+'darkblack',
+'brightblack',
+'vividblack2',
+'darkwhite',
+'brightwhite',
+'subduedpurp',
+'vividpurp',
+'xtremepurp',
+'tamewhite',
+'tameyellow',
+}
+
+for filename in files:
+#    base = filename.split('.')[0]
+    base=filename
+    filename+='.png'
     outfile = f'{base}_bg.png'
 
     base_image = Image.open(filename)
-    base_image.putalpha(mask)
-    base_image.show()
+    base_mask = base_image.getchannel('A')
+    base_mask = ImageChops.multiply(base_mask, mask)
+    base_image.putalpha(base_mask)
+#    base_image.show()
+    base_image.save(outfile)
